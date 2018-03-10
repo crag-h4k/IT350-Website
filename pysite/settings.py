b6fbc1aca109ff6394908c0f19ae9d28c8759915
deadlift1226 <danemorgan91@gmail.com>
2018-03-09 19:23:44 -0500

    everything works!

--- pysite/settings.py
+++ pysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pyadmin',
+#    'pyadmin',
     'pyadmin.apps.PyadminConfig',
 ]
 
